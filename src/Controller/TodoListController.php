f98a16715f1904a3d0c68d8cd10ca408c859ce21
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2020-10-15 21:02:46 +0200

    add signin form

--- src/Controller/TodoListController.php
+++ src/Controller/TodoListController.php
@@ -24,7 +24,7 @@ final class TodoListController extends AbstractController
     public function index(TodoListRepository $todoListRepository): Response
     {
         return $this->render('todo_list/index.html.twig', [
-            'todo_lists' => $todoListRepository->findAll(['user' => $this->getUser()]),
+            'todo_lists' => $todoListRepository->findBy(['user' => $this->getUser()]),
         ]);
     }
 
